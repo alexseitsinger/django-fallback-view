b9e55259a219048e81752275ab9fb5227fbd4525
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-10-03 00:31:40 -0400

    feat: Adds regex for static/ media/ urls.

--- src/fallback_view/utils.py
+++ src/fallback_view/utils.py
@@ -2,6 +2,7 @@ import re
 import os
 from django.urls import get_resolver
 from django.utils.module_loading import import_string
+from django.conf import settings
 
 from .settings import FALLBACK_VIEW
 
@@ -13,14 +14,23 @@ def get_fallback_view():
 def get_unique_urls(resolver=None, prefix=""):
     if resolver is None:
         resolver = get_resolver()
+
     all_urls = []
-    for k,v in resolver.reverse_dict.items():
+
+    for k, v in resolver.reverse_dict.items():
         full = "{}/{}".format(prefix, v[0][0][0])
         full = os.path.normpath(full)
         all_urls.append(full)
-    for k,v in resolver.namespace_dict.items():
+
+    for k, v in resolver.namespace_dict.items():
         all_urls.extend(get_unique_urls(v[1], "{}/{}".format(prefix, v[0])))
-    all_urls = set(all_urls)
+
+    # Add the MEDIA_URL to the list of paths to exclude.
+    all_urls.append("{}%(path)s".format(settings.MEDIA_URL))
+
+    # Create a sorted list.
+    all_urls = sorted(set(all_urls))
+
     return all_urls
 
 
@@ -30,7 +40,7 @@ def get_excluding_regex(unique_urls=None):
     exclude_group = r"(?!^/{}/?$)"
     excluded = []
     for unique_url in unique_urls:
-        unique_url = re.sub(r"\%\(.+\)s", "[\\\w\\\d-]+", unique_url)
+        unique_url = re.sub(r"\%\(.+\)s", r"([\\w\\d_.-]+/?)+", unique_url)
         unique_url = os.path.normpath(unique_url)
         if unique_url.startswith("/"):
             unique_url = unique_url[1:]
@@ -42,5 +52,3 @@ def get_excluding_regex(unique_urls=None):
     joined = "".join(excluded)
     regex = r"{excluded}.+/?$".format(excluded=joined)
     return regex
-
-
